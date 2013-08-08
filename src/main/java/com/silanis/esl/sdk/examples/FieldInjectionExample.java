1a56eede854dd9afc9f4b12541bd9c52a055129e
David Lawson <david_lawson@silanis.com>
2013-08-08 11:51:43 -0400

    Added a sample for field value injection.

--- src/main/java/com/silanis/esl/sdk/examples/FieldInjectionExample.java
+++ src/main/java/com/silanis/esl/sdk/examples/FieldInjectionExample.java
@@ -2,22 +2,18 @@ package com.silanis.esl.sdk.examples;
 
 import com.silanis.esl.sdk.DocumentPackage;
 import com.silanis.esl.sdk.EslClient;
+import com.silanis.esl.sdk.FieldId;
 import com.silanis.esl.sdk.PackageId;
-import com.silanis.esl.sdk.builder.FieldBuilder;
 
 import java.util.Properties;
 
 import static com.silanis.esl.sdk.builder.DocumentBuilder.newDocumentWithName;
-import static com.silanis.esl.sdk.builder.FieldBuilder.signatureDate;
+import static com.silanis.esl.sdk.builder.FieldBuilder.textField;
 import static com.silanis.esl.sdk.builder.PackageBuilder.newPackageNamed;
 import static com.silanis.esl.sdk.builder.SignatureBuilder.signatureFor;
 import static com.silanis.esl.sdk.builder.SignerBuilder.newSignerWithEmail;
 
-/**
- * Extracts the position of a field from the uploaded document
- */
-public class FieldPositionExtraction {
-
+public class FieldInjectionExample {
     private static final Properties props = Props.get();
     public static final String API_KEY = props.getProperty( "api.key" );
     public static final String API_URL = props.getProperty( "api.url" );
@@ -31,13 +27,14 @@ public class FieldPositionExtraction {
                         .withLastName( "Smith" ) )
                 .withDocument( newDocumentWithName( "First Document" )
                         .fromFile( "src/main/resources/document-with-fields.pdf" )
-                        .enableExtraction()
                         .withSignature( signatureFor( props.getProperty( "1.email" ) )
-                                .withName( "AGENT_SIG_1" )
+                                .onPage( 0 )
+                                .atPosition( 100, 100 ) )
+                        .withInjectedField( textField()
+                                .withId( new FieldId( "AGENT_SIG_1" ) )
                                 .withPositionExtracted()
-                                .withField( signatureDate()
-                                        .withPositionExtracted()
-                                        .withName( "AGENT_SIG_2" ) ) ) )
+                                .withName( "AGENT_SIG_1" )
+                                .withValue( "Test Value" ) ) )
                 .build();
 
         PackageId packageId = eslClient.createPackage( superDuperPackage );
