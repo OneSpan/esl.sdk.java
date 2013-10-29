fd98b975748468cff90636a917194583410903f0
David Lawson <david_lawson@silanis.com>
2013-10-29 18:38:26 -0400

    Added support for specifying the name of the configuration file to be loaded as well as creating a basic singleton out of the properties object.

--- src/main/java/com/silanis/esl/sdk/examples/Props.java
+++ src/main/java/com/silanis/esl/sdk/examples/Props.java
@@ -10,13 +10,22 @@ import static com.silanis.esl.sdk.io.Streams.close;
  * Helper class to open the properties file
  */
 public class Props {
+
+    private static Properties instance  = null;
+
     public static Properties get() {
+        return Props.get( "signers.properties" );
+    }
+    synchronized public static Properties get(String filename) {
+        if ( instance != null )
+            return instance;
+
         Properties signers = new Properties();
         InputStream in = null;
 
         try {
-            in = Props.class.getClassLoader().getResourceAsStream( "signers.properties" );
-            if (in == null) throw new IOException( "Could not find signers.properties" );
+            in = Props.class.getClassLoader().getResourceAsStream( filename );
+            if (in == null) throw new IOException( "Could not find properties file: " + filename );
             signers.load( in );
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,6 +33,7 @@ public class Props {
             close(in);
         }
 
+        instance = signers;
         return signers;
     }
 }
