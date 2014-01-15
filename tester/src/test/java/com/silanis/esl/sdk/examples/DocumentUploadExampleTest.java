bd11ebc45452da5507e633535c08bf50ac9631c4
JessicaCai <jessicakuangc@gmail.com>
2014-01-15 15:04:18 -0500

    Test for upload document example.

--- tester/src/test/java/com/silanis/esl/sdk/examples/DocumentUploadExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/DocumentUploadExampleTest.java
@@ -24,7 +24,7 @@ public class DocumentUploadExampleTest {
 
         // Verify if the document was uploaded correctly.
 
-        Document document = documentPackage.getDocument(documentUploadExample.uploadedDocumentName);
+        Document document = documentPackage.getDocument(DocumentUploadExample.uploadedDocumentName);
         byte[] documentBinary = documentUploadExample.eslClient.downloadDocument( documentUploadExample.getPackageId(), document.getId().toString() );
         assertThat("Document was not uploaded correctly. ", documentBinary.length != 0 );
     }
