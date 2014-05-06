5ec8f4256d1139114591934250e1f84d17843895
Lena Hsieh <lena_hsieh@silanis.com>
2014-05-06 14:24:24 -0400

    esl-8317. Added get template list

---
+++ tester/src/test/java/com/silanis/esl/sdk/examples/ListTemplatesExampleTest.java
@@ -0,0 +1,25 @@
+package com.silanis.esl.sdk.examples;
+
+import com.silanis.esl.sdk.DocumentPackage;
+import com.silanis.esl.sdk.Page;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+/**
+ * Created by lena on 2014-05-06.
+ */
+public class ListTemplatesExampleTest {
+
+    @Test
+    public void verifyResult() {
+        ListTemplatesExample example = new ListTemplatesExample(Props.get());
+        example.run();
+
+        Page<DocumentPackage> templateList =  example.getTemplates();
+
+        assertThat("The returned number of templates is less than 1.", templateList.getSize(), is(greaterThanOrEqualTo(1)));
+    }
+}
