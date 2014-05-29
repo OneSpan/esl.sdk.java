ed3543e78ac8d5cc5e8ab7b46f5be4f1fc761cad
Chi-Wing Sit <chiwingsit@gmail.com>
2014-05-29 12:05:45 -0400

    Revert commit changes

--- sdk/src/main/java/com/silanis/esl/sdk/internal/CommunicationException.java
+++ sdk/src/main/java/com/silanis/esl/sdk/internal/CommunicationException.java
@@ -1,24 +1,11 @@
 package com.silanis.esl.sdk.internal;
 
-import com.silanis.esl.api.model.Error;
 import com.silanis.esl.sdk.EslException;
-import com.silanis.esl.sdk.ServerError;
-import com.silanis.esl.sdk.internal.converter.ErrorConverter;
 
 import static java.lang.String.format;
 
 public class CommunicationException extends EslException {
-    public ServerError serverError;
-
     public CommunicationException(String method, String uri, int statusCode, String reasonPhrase, String details) {
         super(format("HTTP %s on URI %s resulted in response with status code: [%d, %s]. Optional details: %s", method, uri, statusCode, reasonPhrase, details));
-
-        com.silanis.esl.api.model.Error error = Serialization.fromJson(details, Error.class);
-
-        serverError = new ErrorConverter(error).toSDKError();
-    }
-
-    public ServerError getServerError(){
-        return serverError;
     }
 }
