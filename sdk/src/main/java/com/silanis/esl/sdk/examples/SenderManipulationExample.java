086e987188195300e40ae4c7b8abeec326d707a7
Lena Hsieh <lena_hsieh@silanis.com>
2014-08-20 15:36:24 -0400

    Fix SenderManipulationExample by increasing page request size

--- sdk/src/main/java/com/silanis/esl/sdk/examples/SenderManipulationExample.java
+++ sdk/src/main/java/com/silanis/esl/sdk/examples/SenderManipulationExample.java
@@ -87,7 +87,5 @@ public class SenderManipulationExample extends SDKSample {
 
         eslClient.getAccountService().updateSender(updatedSenderInfo, accountMembersWithDeletedSender.get(email3).getId());
         accountMembersWithUpdatedSender = eslClient.getAccountService().getSenders(Direction.ASCENDING, new PageRequest(1, 1000));
-
-        System.out.println("# of senders : " + accountMembersWithUpdatedSender.size());
     }
 }
