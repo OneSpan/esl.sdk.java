e84debb25c5db450586b77dccdeda78012aaa4cf
Seung Choi <seung_choi@silanis.com>
2015-05-25 14:33:51 -0400

    17959. Create UpdateTemplateWithPlaceholderExample, UpdateTemplateWithPlaceholderExampleTest for updating template issue

---
+++ sdk/src/main/java/com/silanis/esl/sdk/examples/UpdateTemplateWithPlaceholderExample.java
@@ -0,0 +1,118 @@
+package com.silanis.esl.sdk.examples;
+
+import com.silanis.esl.sdk.DocumentPackage;
+import com.silanis.esl.sdk.DocumentType;
+import com.silanis.esl.sdk.PackageId;
+import com.silanis.esl.sdk.Placeholder;
+import com.silanis.esl.sdk.builder.DocumentBuilder;
+import com.silanis.esl.sdk.builder.SignerBuilder;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import static com.silanis.esl.sdk.builder.PackageBuilder.newPackageNamed;
+import static com.silanis.esl.sdk.builder.SignatureBuilder.signatureFor;
+import static com.silanis.esl.sdk.builder.SignerBuilder.newSignerWithEmail;
+
+/**
+ * Created by schoi on 5/25/15.
+ */
+public class UpdateTemplateWithPlaceholderExample extends SDKSample {
+
+    private InputStream documentInputStream1;
+    private InputStream documentInputStream2;
+
+    public String email1;
+    public String email2;
+    public PackageId templateId;
+
+    public static final String DOCUMENT_NAME = "First Document";
+    public static final String DOCUMENT_ID = "doc1";
+    public static final String TEMPLATE1_NAME = "UpdateTemplateWithPlaceholderExample Template1: " + new SimpleDateFormat("HH:mm:ss").format(new Date());
+    public static final String TEMPLATE2_NAME = "UpdateTemplateWithPlaceholderExample Template2: " + new SimpleDateFormat("HH:mm:ss").format(new Date());
+    public static final String TEMPLATE_DESCRIPTION = "This is a template created using the e-SignLive SDK";
+    public static final String TEMPLATE_EMAIL_MESSAGE = "This message should be delivered to all signers";
+    public static final String TEMPLATE_SIGNER_FIRST = "John";
+    public static final String TEMPLATE_SIGNER_LAST = "Smith";
+
+    public static final String PACKAGE_NAME = "UpdateTemplateWithPlaceholderExample Package: " + new SimpleDateFormat("HH:mm:ss").format(new Date());
+    public static final String PACKAGE_DESCRIPTION = "This is a package created using the e-SignLive SDK";
+
+    public static final String PLACEHOLDER_ID = "PlaceholderId1";
+    public static final String PLACEHOLDER2_ID = "PlaceholderId2";
+
+    public DocumentPackage retrievedTemplate, updatedTemplate;
+
+    public static void main(String... args) {
+        new UpdateTemplateWithPlaceholderExample(Props.get()).run();
+    }
+
+    public UpdateTemplateWithPlaceholderExample(Properties properties) {
+        this(properties.getProperty("api.key"),
+             properties.getProperty("api.url"),
+             properties.getProperty("1.email"),
+             properties.getProperty("2.email"));
+    }
+
+    public UpdateTemplateWithPlaceholderExample(String apiKey, String apiUrl, String email1, String email2) {
+        super(apiKey, apiUrl);
+        this.email1 = email1;
+        this.email2 = email2;
+        documentInputStream1 = this.getClass().getClassLoader().getResourceAsStream("document.pdf");
+        documentInputStream2 = this.getClass().getClassLoader().getResourceAsStream("document.pdf");
+    }
+
+    @Override
+    public void execute() {
+        DocumentPackage template1 = newPackageNamed(TEMPLATE1_NAME)
+                .describedAs(TEMPLATE_DESCRIPTION)
+                .withEmailMessage(TEMPLATE_EMAIL_MESSAGE)
+                .withSigner(newSignerWithEmail(email1)
+                                    .withFirstName(TEMPLATE_SIGNER_FIRST)
+                                    .withLastName(TEMPLATE_SIGNER_LAST))
+                .withSigner(SignerBuilder.newSignerPlaceholder(new Placeholder(PLACEHOLDER_ID)))
+                .withDocument(DocumentBuilder.newDocumentWithName(DOCUMENT_NAME)
+                    .withId(DOCUMENT_ID)
+                    .fromStream(documentInputStream1, DocumentType.PDF)
+                    .withSignature(signatureFor(email1)
+                                           .onPage(0)
+                                           .atPosition(100, 100))
+                    .withSignature(signatureFor(new Placeholder(PLACEHOLDER_ID))
+                                           .onPage(0)
+                                           .atPosition(400, 100)))
+                .build();
+
+        DocumentPackage template2 = newPackageNamed(TEMPLATE2_NAME)
+                .describedAs(TEMPLATE_DESCRIPTION)
+                .withEmailMessage(TEMPLATE_EMAIL_MESSAGE)
+                .withSigner(newSignerWithEmail(email1)
+                                    .withFirstName(TEMPLATE_SIGNER_FIRST)
+                                    .withLastName(TEMPLATE_SIGNER_LAST))
+                .withSigner(SignerBuilder.newSignerPlaceholder(new Placeholder(PLACEHOLDER_ID)))
+                .withSigner(SignerBuilder.newSignerPlaceholder(new Placeholder(PLACEHOLDER2_ID)))
+                .withDocument(DocumentBuilder.newDocumentWithName(DOCUMENT_NAME)
+                    .withId(DOCUMENT_ID)
+                    .fromStream(documentInputStream2, DocumentType.PDF)
+                    .withSignature(signatureFor(email1)
+                                           .onPage(0)
+                                           .atPosition(100, 100))
+                    .withSignature(signatureFor(new Placeholder(PLACEHOLDER_ID))
+                                           .onPage(0)
+                                           .atPosition(400, 100))
+                    .withSignature(signatureFor(new Placeholder(PLACEHOLDER2_ID))
+                                           .onPage(0)
+                                           .atPosition(500, 100)))
+                .build();
+
+        templateId = eslClient.getTemplateService().createTemplate(template1);
+
+        retrievedTemplate = eslClient.getPackage(templateId);
+        template2.setId(templateId);
+
+        eslClient.getTemplateService().updateTemplate(template2);
+
+        updatedTemplate = eslClient.getPackage(templateId);
+    }
+}
---
+++ tester/src/test/java/com/silanis/esl/sdk/examples/UpdateTemplateWithPlaceholderExampleTest.java
@@ -0,0 +1,32 @@
+package com.silanis.esl.sdk.examples;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by schoi on 5/25/15.
+ */
+public class UpdateTemplateWithPlaceholderExampleTest {
+
+    @Test
+    public void verifyResult() {
+
+        UpdateTemplateWithPlaceholderExample example = new UpdateTemplateWithPlaceholderExample( Props.get() );
+        example.run();
+
+        assertThat("Template is not created correctly.", example.retrievedTemplate.getName(), is(example.TEMPLATE1_NAME));
+        assertThat("Template is not created correctly.", example.retrievedTemplate.getSigners().size(), is(2));
+        assertThat("Template is not created correctly.", example.retrievedTemplate.getPlaceholders().size(), is(1));
+        assertThat("Template is not created correctly.", example.retrievedTemplate.getPlaceholders().get(example.PLACEHOLDER_ID), is(not(nullValue())));
+
+        assertThat("Template is not updated correctly.", example.retrievedTemplate.getName(), is(example.TEMPLATE2_NAME));
+        assertThat("Template is not updated correctly.", example.retrievedTemplate.getSigners().size(), is(2));
+        assertThat("Template is not updated correctly.", example.retrievedTemplate.getPlaceholders().size(), is(2));
+        assertThat("Template is not updated correctly.", example.retrievedTemplate.getPlaceholders().get(example.PLACEHOLDER_ID), is(not(nullValue())));
+        assertThat("Template is not updated correctly.", example.retrievedTemplate.getPlaceholders().get(example.PLACEHOLDER2_ID), is(not(nullValue())));
+    }
+}
