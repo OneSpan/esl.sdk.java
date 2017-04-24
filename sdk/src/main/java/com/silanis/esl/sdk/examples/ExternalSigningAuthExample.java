dda7a9c7ff905a64a8eed641c16ffa9e1d909d4d
diazos1 <oscar.diaz@esignlive.com>
2017-04-24 16:04:21 -0400

    PB-921 Added ExternalSigning Example

---
+++ sdk/src/main/java/com/silanis/esl/sdk/examples/ExternalSigningAuthExample.java
@@ -0,0 +1,38 @@
+package com.silanis.esl.sdk.examples;
+
+
+import com.silanis.esl.sdk.DocumentPackage;
+import com.silanis.esl.sdk.DocumentType;
+import com.silanis.esl.sdk.builder.*;
+
+public class ExternalSigningAuthExample extends SDKSample {
+
+    public static String PROVIDER_KEY = "DIGIPASS";
+    public static String IDENTITY_INFO = "Xz3AwPp9xazJ0ku5CZnlmgAx2DlJJGw0k0kd8SHkAeT";
+
+    public static void main(String... args) {
+        new ExternalSigningAuthExample().run();
+    }
+
+
+    @Override
+    protected void execute() {
+
+        DocumentPackage packageTest = PackageBuilder.newPackageNamed(packageName)
+                .withSigner(SignerBuilder.newSignerWithEmail(email1)
+                        .withFirstName("John1")
+                        .withLastName("Smith1"))
+                .withSigner(SignerBuilder.newSignerWithEmail(email2).
+                        withFirstName("John2").withLastName("Smith2")
+                        .withExternalSigningAuth(ExternalSigningAuthBuilder.forProvider(PROVIDER_KEY)
+                                .withIdentityInfo(IDENTITY_INFO)))
+                .withDocument(DocumentBuilder.newDocumentWithName("Custom Consent Document")
+                        .fromStream(documentInputStream1, DocumentType.PDF)
+                        .withSignature(SignatureBuilder.signatureFor(email1).onPage(0).atPosition(100, 100))).build();
+
+        packageId = eslClient.createPackage(packageTest);
+        retrievedPackage = eslClient.getPackage(packageId);
+
+
+    }
+}
