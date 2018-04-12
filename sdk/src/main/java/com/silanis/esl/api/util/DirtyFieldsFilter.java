3dc384357597c5436bb2cc7ac50f642195010ac9
john <seung.choi@silanis.com>
2018-04-12 12:05:24 -0400

    PB-10209. Fix Security vulnerability issue

--- sdk/src/main/java/com/silanis/esl/api/util/DirtyFieldsFilter.java
+++ sdk/src/main/java/com/silanis/esl/api/util/DirtyFieldsFilter.java
@@ -1,7 +1,10 @@
 package com.silanis.esl.api.util;
 
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.ser.BeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
 import com.silanis.esl.api.model.Model;
@@ -34,6 +37,14 @@ public class DirtyFieldsFilter implements BeanPropertyFilter {
                 paramBeanPropertyWriter.serializeAsField(paramObject, paramJsonGenerator, paramSerializerProvider);
             }
         }
+    }
+
+    @Override
+    public void depositSchemaProperty(BeanPropertyWriter beanPropertyWriter, ObjectNode objectNode, SerializerProvider serializerProvider) throws JsonMappingException {
+    }
+
+    @Override
+    public void depositSchemaProperty(BeanPropertyWriter beanPropertyWriter, JsonObjectFormatVisitor jsonObjectFormatVisitor, SerializerProvider serializerProvider) throws JsonMappingException {
     }
 
 }
