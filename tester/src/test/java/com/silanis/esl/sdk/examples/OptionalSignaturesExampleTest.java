e89c1a5f6994a0544a7028eb169ec1f6af34722c
Marcin Sliwinski <Marcin.Sliwinski@vasco.com>
2018-05-23 10:27:55 +0200

    PB-11322: Added support for optional signatures.

---
+++ tester/src/test/java/com/silanis/esl/sdk/examples/OptionalSignaturesExampleTest.java
@@ -0,0 +1,24 @@
+package com.silanis.esl.sdk.examples;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class OptionalSignaturesExampleTest {
+
+    @Test
+    public void verifyResult() {
+        OptionalSignaturesExample example = new OptionalSignaturesExample();
+        example.run();
+
+        assertThat("signer1's signable signatures are not set correctly. ", example.signer1SignableSignatures.size(), hasSize(2));
+        assertThat("signer1's signable signature1 is not set correctly. ", example.signer1SignableSignatures.get(0).getSignerEmail(), is(example.email1));
+        assertThat("signer1's signable signature2 is not set correctly. ", example.signer1SignableSignatures.get(1).getSignerEmail(), is(example.email1));
+
+        assertThat("signer2's signable signatures are not set correctly. ", example.signer2SignableSignatures.size(), hasSize(2));
+        assertThat("signer2's signable signature1 is not set correctly. ", example.signer2SignableSignatures.get(0).getSignerEmail(), is(example.email2));
+        assertThat("signer2's signable signature2 is not set correctly. ", example.signer2SignableSignatures.get(1).getSignerEmail(), is(example.email2));
+        assertTrue("signer2's signable signature2 is not optional. ", example.signer2SignableSignatures.get(1).isOptional());
+    }
+}
