1801a294983e30241d737f939484c32b845cdcc3
Marcin Sliwinski <Marcin.Sliwinski@vasco.com>
2018-05-30 14:35:02 +0200

    Additional fixes as per review comments

--- tester/src/test/java/com/silanis/esl/sdk/examples/OptionalSignaturesExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/OptionalSignaturesExampleTest.java
@@ -12,11 +12,11 @@ public class OptionalSignaturesExampleTest {
         OptionalSignaturesExample example = new OptionalSignaturesExample();
         example.run();
 
-        assertThat("signer1's signable signatures are not set correctly. ", example.signer1SignableSignatures.size(), is(2));
+        assertThat("signer1's signable signatures are not set correctly. ", example.signer1SignableSignatures.size(), hasSize(2));
         assertThat("signer1's signable signature1 is not set correctly. ", example.signer1SignableSignatures.get(0).getSignerEmail(), is(example.email1));
         assertThat("signer1's signable signature2 is not set correctly. ", example.signer1SignableSignatures.get(1).getSignerEmail(), is(example.email1));
 
-        assertThat("signer2's signable signatures are not set correctly. ", example.signer2SignableSignatures.size(), is(2));
+        assertThat("signer2's signable signatures are not set correctly. ", example.signer2SignableSignatures.size(), hasSize(2));
         assertThat("signer2's signable signature1 is not set correctly. ", example.signer2SignableSignatures.get(0).getSignerEmail(), is(example.email2));
         assertThat("signer2's signable signature2 is not set correctly. ", example.signer2SignableSignatures.get(1).getSignerEmail(), is(example.email2));
         assertTrue("signer2's signable signature2 is not optional. ", example.signer2SignableSignatures.get(1).isOptional());
