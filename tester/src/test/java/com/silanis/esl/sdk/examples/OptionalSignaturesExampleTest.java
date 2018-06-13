b678b2c051aa6c054566292c56dc6c8c5c198d2c
john <seung.choi@silanis.com>
2018-06-13 09:53:48 -0400

    PB-13092. Fix bugs in OptionalSignaturesExampleTest

--- tester/src/test/java/com/silanis/esl/sdk/examples/OptionalSignaturesExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/OptionalSignaturesExampleTest.java
@@ -3,7 +3,10 @@ package com.silanis.esl.sdk.examples;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class OptionalSignaturesExampleTest {
 
@@ -12,13 +15,18 @@ public class OptionalSignaturesExampleTest {
         OptionalSignaturesExample example = new OptionalSignaturesExample();
         example.run();
 
-        assertThat("signer1's signable signatures are not set correctly. ", example.signer1SignableSignatures.size(), hasSize(2));
+        assertThat("signer1's signable signatures are not set correctly. ", example.signer1SignableSignatures, hasSize(2));
         assertThat("signer1's signable signature1 is not set correctly. ", example.signer1SignableSignatures.get(0).getSignerEmail(), is(example.email1));
         assertThat("signer1's signable signature2 is not set correctly. ", example.signer1SignableSignatures.get(1).getSignerEmail(), is(example.email1));
 
-        assertThat("signer2's signable signatures are not set correctly. ", example.signer2SignableSignatures.size(), hasSize(2));
+        assertThat("signer2's signable signatures are not set correctly. ", example.signer2SignableSignatures, hasSize(2));
         assertThat("signer2's signable signature1 is not set correctly. ", example.signer2SignableSignatures.get(0).getSignerEmail(), is(example.email2));
         assertThat("signer2's signable signature2 is not set correctly. ", example.signer2SignableSignatures.get(1).getSignerEmail(), is(example.email2));
-        assertTrue("signer2's signable signature2 is not optional. ", example.signer2SignableSignatures.get(1).isOptional());
+
+        if(example.signer2SignableSignatures.get(0).isOptional()) {
+            assertFalse("signer2's signable signature2 is not optional. ", example.signer2SignableSignatures.get(1).isOptional());
+        } else {
+            assertTrue("signer2's signable signature2 is not optional. ", example.signer2SignableSignatures.get(1).isOptional());
+        }
     }
 }
