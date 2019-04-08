3c210f93108cac5bfe5573fab95abd83019a3f73
john <seung.choi@silanis.com>
2019-04-08 11:13:50 -0400

    PB-25503. Remove the assert checking null value for radio.

--- tester/src/test/java/com/silanis/esl/sdk/examples/CreatePackageFromTemplateWithFieldsExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/CreatePackageFromTemplateWithFieldsExampleTest.java
@@ -9,7 +9,8 @@ import static com.silanis.esl.sdk.FieldStyle.*;
 import static com.silanis.esl.sdk.builder.FieldBuilder.CHECKBOX_CHECKED;
 import static com.silanis.esl.sdk.builder.FieldBuilder.RADIO_SELECTED;
 import static com.silanis.esl.sdk.examples.CreatePackageFromTemplateWithFieldsExample.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -25,49 +26,48 @@ public class CreatePackageFromTemplateWithFieldsExampleTest {
 
         for (Signature signature : documentPackage.getDocument(DOCUMENT_NAME).getSignatures()) {
             for (Field field : signature.getFields()) {
-                if(field.getId().toString().equals(TEXTFIELD_ID)){
-                    assertThat(field.getPage(), equalTo(TEXTFIELD_PAGE));
-                    assertThat(field.getStyle(), equalTo(UNBOUND_TEXT_FIELD));
+                if (field.getId().toString().equals(TEXTFIELD_ID)) {
+                    assertThat(field.getPage(), is(TEXTFIELD_PAGE));
+                    assertThat(field.getStyle(), is(UNBOUND_TEXT_FIELD));
                 }
                 if (field.getId().toString().equals(CHECKBOX_1_ID)) {
-                    assertThat(field.getPage(), equalTo(CHECKBOX_1_PAGE));
-                    assertThat(field.getStyle(), equalTo(UNBOUND_CHECK_BOX));
+                    assertThat(field.getPage(), is(CHECKBOX_1_PAGE));
+                    assertThat(field.getStyle(), is(UNBOUND_CHECK_BOX));
                     assertThat(field.getValue(), nullValue());
                 }
                 if (field.getId().toString().equals(CHECKBOX_2_ID)) {
-                    assertThat(field.getPage(), equalTo(CHECKBOX_1_PAGE));
-                    assertThat(field.getStyle(), equalTo(UNBOUND_CHECK_BOX));
-                    assertThat(field.getValue(), equalTo(CHECKBOX_CHECKED));
+                    assertThat(field.getPage(), is(CHECKBOX_1_PAGE));
+                    assertThat(field.getStyle(), is(UNBOUND_CHECK_BOX));
+                    assertThat(field.getValue(), is(CHECKBOX_CHECKED));
                 }
                 if (field.getId().toString().equals(RADIO_1_ID)) {
-                    assertThat(field.getPage(), equalTo(RADIO_1_PAGE));
-                    assertThat(field.getStyle(), equalTo(UNBOUND_RADIO_BUTTON));
-                    assertThat(field.getFieldValidator().getOptions().get(0), equalTo(RADIO_1_GROUP));
-                    assertThat(field.getValue(), nullValue());
+                    assertThat(field.getPage(), is(RADIO_1_PAGE));
+                    assertThat(field.getStyle(), is(UNBOUND_RADIO_BUTTON));
+                    assertThat(field.getFieldValidator().getOptions().get(0), is(RADIO_1_GROUP));
                 }
                 if (field.getId().toString().equals(RADIO_2_ID)) {
-                    assertThat(field.getPage(), equalTo(RADIO_2_PAGE));
-                    assertThat(field.getStyle(), equalTo(UNBOUND_RADIO_BUTTON));
-                    assertThat(field.getFieldValidator().getOptions().get(0), equalTo(RADIO_2_GROUP));
-                    assertThat(field.getValue(), equalTo(RADIO_SELECTED));
+                    assertThat(field.getPage(), is(RADIO_2_PAGE));
+                    assertThat(field.getStyle(), is(UNBOUND_RADIO_BUTTON));
+                    assertThat(field.getFieldValidator().getOptions().get(0), is(RADIO_2_GROUP));
+                    assertThat(field.getValue(), is(RADIO_SELECTED));
                 }
                 if (field.getId().toString().equals(DROP_LIST_ID)) {
-                    assertThat(field.getPage(), equalTo(DROP_LIST_PAGE));
-                    assertThat(field.getStyle(), equalTo(DROP_LIST));
-                    assertThat(field.getFieldValidator().getOptions().get(0), equalTo(DROP_LIST_OPTION1));
-                    assertThat(field.getFieldValidator().getOptions().get(1), equalTo(DROP_LIST_OPTION2));
-                    assertThat(field.getFieldValidator().getOptions().get(2), equalTo(DROP_LIST_OPTION3));
-                    assertThat(field.getValue(), equalTo(DROP_LIST_OPTION2));
+                    assertThat(field.getPage(), is(DROP_LIST_PAGE));
+                    assertThat(field.getStyle(), is(DROP_LIST));
+                    assertThat(field.getFieldValidator().getOptions().get(0), is(DROP_LIST_OPTION1));
+                    assertThat(field.getFieldValidator().getOptions().get(1), is(DROP_LIST_OPTION2));
+                    assertThat(field.getFieldValidator().getOptions().get(2), is(DROP_LIST_OPTION3));
+                    assertThat(field.getValue(), is(DROP_LIST_OPTION2));
                 }
                 if (field.getId().toString().equals(TEXT_AREA_ID)) {
-                    assertThat(field.getPage(), equalTo(TEXT_AREA_PAGE));
-                    assertThat(field.getStyle(), equalTo(TEXT_AREA));
-                    assertThat(field.getValue(), equalTo(TEXT_AREA_VALUE));
+                    assertThat(field.getPage(), is(TEXT_AREA_PAGE));
+                    assertThat(field.getStyle(), is(TEXT_AREA));
+                    assertThat(field.getValue(), is(TEXT_AREA_VALUE));
                 }
                 if (field.getId().toString().equals(LABEL_ID)) {
-                    assertThat(field.getPage(), equalTo(LABEL_PAGE));
-                    assertThat(field.getStyle(), equalTo(LABEL));
-                    assertThat(field.getValue(), equalTo(LABEL_VALUE));
+                    assertThat(field.getPage(), is(LABEL_PAGE));
+                    assertThat(field.getStyle(), is(LABEL));
+                    assertThat(field.getValue(), is(LABEL_VALUE));
                 }
             }
         }
