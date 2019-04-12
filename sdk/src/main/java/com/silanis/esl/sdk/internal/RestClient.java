475f65e184c50e3da4abf7d89313c898f01238a8
john <seung.choi@silanis.com>
2019-04-12 10:33:50 -0400

    CI-583. Update version

--- sdk/src/main/java/com/silanis/esl/sdk/internal/RestClient.java
+++ sdk/src/main/java/com/silanis/esl/sdk/internal/RestClient.java
@@ -54,7 +54,7 @@ public class RestClient {
 
     public static final String CHARSET_UTF_8 = "UTF-8";
 
-    public static final String ESL_API_VERSION = "11.25";
+    public static final String ESL_API_VERSION = "11.26";
     public static final String ESL_API_VERSION_HEADER = "esl-api-version=" + ESL_API_VERSION;
 
     public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
