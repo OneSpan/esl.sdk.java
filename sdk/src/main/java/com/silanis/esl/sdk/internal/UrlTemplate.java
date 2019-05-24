50c5c6531e10f65bbbfe4d0cace0fb79ba46a1e7
mateusz <mateusz.gruszka@vasco.com>
2019-05-24 15:27:12 +0200

    PB-27291 Ability to return transaction GUIDs only

--- sdk/src/main/java/com/silanis/esl/sdk/internal/UrlTemplate.java
+++ sdk/src/main/java/com/silanis/esl/sdk/internal/UrlTemplate.java
@@ -14,6 +14,7 @@ public class UrlTemplate {
     // Package Service
     public static final String PACKAGE_PATH = "/packages";
     public static final String PACKAGE_LIST_PATH = "/packages?query={status}&from={from}&to={to}";
+    public static final String PACKAGE_FIELDS_LIST_PATH = "/packages?query={status}&from={from}&to={to}&fields={fields}";
     public static final String PACKAGE_LIST_STATUS_DATE_RANGE_PATH = "/packages?query={status}&from={from}&to={to}&lastUpdatedStartDate={lastUpdatedStartDate}&lastUpdatedEndDate={lastUpdatedEndDate}";
     public static final String PACKAGE_ID_PATH = "/packages/{packageId}";
     public static final String DOCUMENT_PATH = "/packages/{packageId}/documents";
