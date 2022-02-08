8a342a86d4f09481322277d3cc873485cf822ffe
zbi <zhichen.bi@onespan.com>
2022-02-07 20:55:18 -0500

    PB-78896 Support Self-Serve

--- tester/src/test/java/com/silanis/esl/sdk/examples/AccountSettingsExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/AccountSettingsExampleTest.java
@@ -22,12 +22,6 @@ public class AccountSettingsExampleTest {
         assertTrue("'defaultTimeBasedExpiry' in AccountPackageSettings should be updated correctly", example.patchedAccountSettings.getAccountPackageSettings().getDefaultTimeBasedExpiry());
         assertTrue("'disableDeclineOther' in AccountPackageSettings should be updated correctly", example.patchedAccountSettings.getAccountPackageSettings().getDisableDeclineOther());
 
-        assertFalse("'ada' in AccountPackageSettings should be reset to default", example.deletedAccountSettings.getAccountPackageSettings().getAda());
-        assertFalse("'declineButton' in AccountPackageSettings should be reset to default", example.deletedAccountSettings.getAccountPackageSettings().getDeclineButton());
-        assertFalse("'defaultTimeBasedExpiry' in AccountPackageSettings should be reset to default", example.deletedAccountSettings.getAccountPackageSettings().getDefaultTimeBasedExpiry());
-        assertFalse("'DisableDeclineOther' in AccountPackageSettings should be reset to default", example.deletedAccountSettings.getAccountPackageSettings().getDisableDeclineOther());
-
-
         assertNotNull("'allowCheckboxConsentApproval' in AccountFeatureSettings should be returned", example.defaultAccountSettings.getAccountFeatureSettings().getAllowCheckboxConsentApproval());
         assertNotNull("'allowInPersonForAccountSenders' in AccountFeatureSettings should be returned", example.defaultAccountSettings.getAccountFeatureSettings().getAllowInPersonForAccountSenders());
         assertNotNull("'Attachments' in AccountFeatureSettings should be returned", example.defaultAccountSettings.getAccountFeatureSettings().getAttachments());
@@ -37,11 +31,6 @@ public class AccountSettingsExampleTest {
         assertTrue("'allowInPersonForAccountSenders' in AccountFeatureSettings should be updated correctly", example.patchedAccountSettings.getAccountFeatureSettings().getAllowInPersonForAccountSenders());
         assertFalse("'Attachments' in AccountFeatureSettings should be updated correctly", example.patchedAccountSettings.getAccountFeatureSettings().getAttachments());
         assertFalse("'ConditionalFields' in AccountFeatureSettings should be updated correctly", example.patchedAccountSettings.getAccountFeatureSettings().getConditionalFields());
-
-        assertTrue("'allowCheckboxConsentApproval' in AccountFeatureSettings should be reset to default", example.deletedAccountSettings.getAccountFeatureSettings().getAllowCheckboxConsentApproval());
-        assertFalse("'allowInPersonForAccountSenders' in AccountFeatureSettings should be reset to default", example.deletedAccountSettings.getAccountFeatureSettings().getAllowInPersonForAccountSenders());
-        assertTrue("'Attachments' in AccountFeatureSettings should be reset to default", example.deletedAccountSettings.getAccountFeatureSettings().getAttachments());
-        assertTrue("'ConditionalFields' in AccountFeatureSettings should be reset todefault", example.deletedAccountSettings.getAccountFeatureSettings().getConditionalFields());
 
     }
 }
