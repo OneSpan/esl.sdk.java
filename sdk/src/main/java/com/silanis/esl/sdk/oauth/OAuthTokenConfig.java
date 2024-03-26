0955f65fd5e15483fde1944a26d7be1ebb674828
gadheam1 <amit.gadhekar@onespan.com>
2024-03-26 09:38:35 -0400

    Generate new oAuth2 token before expiry

--- sdk/src/main/java/com/silanis/esl/sdk/oauth/OAuthTokenConfig.java
+++ sdk/src/main/java/com/silanis/esl/sdk/oauth/OAuthTokenConfig.java
@@ -1,44 +1,26 @@
 package com.silanis.esl.sdk.oauth;
 
 import com.silanis.esl.sdk.internal.Asserts;
-import org.apache.commons.lang3.StringUtils;
 
 public class OAuthTokenConfig {
-    private final String baseUrl;
-
     private final String grantType;
-
     private final String clientId;
     private final String clientSecret;
-    private final TokenType tokenType;
-    private final String senderEmail;
     private final String authenticationServer;
 
-    private final String scope;
-
     private OAuthTokenConfig(Builder builder) {
-        this.tokenType = builder.tokenType;
-
-        this.baseUrl = builder.baseUrl;
         this.authenticationServer = builder.authenticationServer;
         this.clientId = builder.clientId;
         this.clientSecret = builder.clientSecret;
-        this.senderEmail = builder.senderEmail;
-        this.scope = builder.scope;
         this.grantType = builder.grantType;
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public String getAuthenticationURL() {
-        return String.format("%s?grant_type=%s",getAuthenticationServer(), getGrantType());
-    }
-
-
-    public enum TokenType {
-        OWNER,SENDER
-    }
-
-    public String getBaseUrl() {
-        return baseUrl;
+        return String.format("%s?grant_type=%s", getAuthenticationServer(), getGrantType());
     }
 
     public String getClientId() {
@@ -49,61 +31,31 @@ public class OAuthTokenConfig {
         return clientSecret;
     }
 
-    public TokenType getTokenType() {
-        return tokenType;
-    }
-
-    public String getSenderEmail() {
-        return senderEmail;
-    }
-
     public String getAuthenticationServer() {
         return authenticationServer;
     }
 
-    public String getGrantType() { return grantType;}
-
-    public String getScope() { return scope;}
+    public String getGrantType() {
+        return grantType;
+    }
 
     @Override
     public String toString() {
 
-        return String.format("OAuthTokenConfig{ '%s':'%s', '%s': '%s', '%s': '%s', '%s': '%s','%s': '%s', '%s': '%s','%s': '%s'}",
-                "baseUrl",baseUrl,
-                "authenticationServerUrl", authenticationServer,
-                "scope", scope,
-                "clientId'", clientId,
-                "clientSecret", "*********",
-                "tokenType", tokenType,
-                "senderEmail", senderEmail);
+        return String.format("OAuthTokenConfig{ '%s':'%s', '%s': '%s', '%s': '%s'}",
+            "authenticationServerUrl", authenticationServer,
+            "clientId'", clientId,
+            "clientSecret", "*********");
     }
 
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static final class  Builder {
-        private String baseUrl;
-
+    public static final class Builder {
         private String grantType;
-
         private String clientId;
         private String clientSecret;
-        private TokenType tokenType;
-        private String senderEmail;
         private String authenticationServer;
 
-        private String scope;
-
         public Builder() {
-            tokenType = TokenType.OWNER;
-            scope = "user.read,user.write";
-            grantType ="client_credentials";
-        }
-
-        public Builder withBaseURL(String baseUrl) {
-            this.baseUrl = baseUrl;
-            return this;
+            grantType = "client_credentials";
         }
 
         public Builder withClientId(String clientId) {
@@ -116,38 +68,17 @@ public class OAuthTokenConfig {
             return this;
         }
 
-        public Builder withSenderEmail(String senderEmail) {
-            this.senderEmail = senderEmail;
-            return this;
-        }
-
 
         public Builder withAuthenticationServer(String authenticationServer) {
             this.authenticationServer = authenticationServer;
             return this;
         }
 
-        public Builder withScope(String scope) {
-            if  (StringUtils.isNotBlank(scope)) {
-                this.scope = scope;
-            }
-            return this;
-        }
-
-
         public OAuthTokenConfig build() {
-            Asserts.notNullOrEmpty(baseUrl, "baseUrl");
             Asserts.notNullOrEmpty(clientId, "clientId");
             Asserts.notNullOrEmpty(clientSecret, "clientSecret");
-            Asserts.notNull(tokenType, "tokenType");
             Asserts.notNullOrEmpty(authenticationServer, "authenticationServer");
-            Asserts.notNull(scope, "scope");
             Asserts.notNullOrEmpty(grantType, "grantType");
-
-            if (tokenType == TokenType.SENDER) {
-                Asserts.notNull(senderEmail, "senderEmail");
-            }
-
             return new OAuthTokenConfig(this);
         }
 
