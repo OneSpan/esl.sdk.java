db5e319d9b2b5f406f6042df0e6601ed3521d5fe
nikrama1 <mahdiye.nikraftar@onespan.com>
2025-07-07 15:46:16 -0400

    feat: PB-116799 Support in app widget self-serve configuration in SDKs

--- sdk/src/main/java/com/silanis/esl/sdk/examples/AccountSettingsExample.java
+++ sdk/src/main/java/com/silanis/esl/sdk/examples/AccountSettingsExample.java
@@ -43,7 +43,7 @@ public class AccountSettingsExample extends SDKSample {
                         .withOverrideRecipientsPreferredLanguage()
                         .withEnableRecipientHistory()
                         .withAllowSignersDownloadEvidenceSummary()
-                        .withDocumentWidget())
+                        .withoutDocumentWidget())
                 .build();
 
         //Save account settings
