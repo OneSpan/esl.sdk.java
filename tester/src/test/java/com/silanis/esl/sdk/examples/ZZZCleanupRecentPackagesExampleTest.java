6c40f3ae692df0c4e720a43a2f734b149745176b
houjianyu <william.jianyuhou@onespan.com>
2026-04-22 15:04:52 -0400

    PB-122275 OSS 26.R3 (11.68.0) Publish Java SDK and Misc.

--- tester/src/test/java/com/silanis/esl/sdk/examples/ZZZCleanupRecentPackagesExampleTest.java
+++ tester/src/test/java/com/silanis/esl/sdk/examples/ZZZCleanupRecentPackagesExampleTest.java
@@ -26,11 +26,14 @@ public class ZZZCleanupRecentPackagesExampleTest {
         assertThat("Deleted package IDs list should not be null", example.deletedPackageIds, notNullValue());
         assertThat("Deleted template IDs list should not be null", example.deletedTemplateIds, notNullValue());
         assertThat("Deleted sender IDs list should not be null", example.deletedSenderIds, notNullValue());
+        assertThat("Deleted group IDs list should not be null", example.deletedGroupIds, notNullValue());
         assertThat("Deleted packages count should be non-negative", example.deletedPackagesCount, is(greaterThanOrEqualTo(0)));
         assertThat("Deleted templates count should be non-negative", example.deletedTemplatesCount, is(greaterThanOrEqualTo(0)));
         assertThat("Deleted senders count should be non-negative", example.deletedSendersCount, is(greaterThanOrEqualTo(0)));
+        assertThat("Deleted groups count should be non-negative", example.deletedGroupsCount, is(greaterThanOrEqualTo(0)));
         assertThat("Deleted packages count should match ID list size", example.deletedPackagesCount, is(example.deletedPackageIds.size()));
         assertThat("Deleted templates count should match ID list size", example.deletedTemplatesCount, is(example.deletedTemplateIds.size()));
         assertThat("Deleted senders count should match ID list size", example.deletedSendersCount, is(example.deletedSenderIds.size()));
+        assertThat("Deleted groups count should match ID list size", example.deletedGroupsCount, is(example.deletedGroupIds.size()));
     }
 }
