75f5a75556823b0f50f0eac8268e1ae9223c91b5
nikrama1 <mahdiye.nikraftar@onespan.com>
2026-08-25 11:10:28 -0400

    feat: PB-130536 expose the clickable-area property

---
+++ sdk/src/main/java/com/silanis/esl/sdk/internal/converter/ClickableAreaConverter.java
@@ -0,0 +1,76 @@
+package com.silanis.esl.sdk.internal.converter;
+
+import com.silanis.esl.sdk.ClickableArea;
+import com.silanis.esl.sdk.ClickableAreaAlignment;
+
+/**
+ * Converter between SDK ClickableArea and API FieldClickableArea.
+ */
+public class ClickableAreaConverter {
+
+    private ClickableArea sdkClickableArea = null;
+    private com.silanis.esl.api.model.FieldClickableArea apiClickableArea = null;
+
+    /**
+     * Construct with SDK object involved in conversion.
+     *
+     * @param clickableArea
+     */
+    public ClickableAreaConverter(ClickableArea clickableArea) {
+        this.sdkClickableArea = clickableArea;
+    }
+
+    /**
+     * Construct with API object involved in conversion.
+     *
+     * @param clickableArea
+     */
+    public ClickableAreaConverter(com.silanis.esl.api.model.FieldClickableArea clickableArea) {
+        this.apiClickableArea = clickableArea;
+    }
+
+    /**
+     * Convert from SDK to API object.
+     *
+     * @return a FieldClickableArea object.
+     */
+    public com.silanis.esl.api.model.FieldClickableArea toAPIFieldClickableArea() {
+
+        if (sdkClickableArea == null) {
+            return apiClickableArea;
+        }
+
+        com.silanis.esl.api.model.FieldClickableArea result = new com.silanis.esl.api.model.FieldClickableArea();
+
+        result.safeSetWidth(sdkClickableArea.getWidth());
+        result.safeSetHeight(sdkClickableArea.getHeight());
+        if (sdkClickableArea.getAlignment() != null) {
+            result.setAlignment(sdkClickableArea.getAlignment().name());
+        }
+
+        return result;
+    }
+
+    /**
+     * Convert from API to SDK object.
+     *
+     * @return a ClickableArea object.
+     */
+    public ClickableArea toSDKClickableArea() {
+
+        if (apiClickableArea == null) {
+            return sdkClickableArea;
+        }
+
+        ClickableArea result = new ClickableArea();
+
+        if ( apiClickableArea.getWidth() != null )
+            result.setWidth(apiClickableArea.getWidth());
+        if ( apiClickableArea.getHeight() != null )
+            result.setHeight(apiClickableArea.getHeight());
+        if ( apiClickableArea.getAlignment() != null )
+            result.setAlignment(ClickableAreaAlignment.valueOf(apiClickableArea.getAlignment()));
+
+        return result;
+    }
+}
