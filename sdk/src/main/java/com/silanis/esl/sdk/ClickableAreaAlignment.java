ef280d28a8f6ed68bd1fde8bcb600c9eab715f05
nikrama1 <mahdiye.nikraftar@onespan.com>
2026-09-04 11:06:40 -0400

    feat: PB-130536 expose the clickable-area property (#5)
    * feat: PB-130536 expose the clickable-area property
    
    * feat: PB-130536 add clickable-area SDK examples
    
    * feat: PB-130536 guard clickable-area example tests against null when the flag is disabled
    
    * feat: PB-130536 remove ClickableAreaCloneExample and its test
    
    * feat: PB-130536 use simple class names instead of fully-qualified names in ClickableAreaConverter
    
    * feat: PB-130536 expose the clickable-area property

---
+++ sdk/src/main/java/com/silanis/esl/sdk/ClickableAreaAlignment.java
@@ -0,0 +1,16 @@
+package com.silanis.esl.sdk;
+
+/**
+ * The anchor position of a field's mark within its clickable area.
+ */
+public enum ClickableAreaAlignment {
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    CENTER_LEFT,
+    CENTER,
+    CENTER_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT
+}
